979579c332d208ac929c8bdca3717c0ccc100d6d
davi <davimqz@proton.me>
2024-05-18 12:44:23 -0300

    [feat] adicionando funcoes CRUD

--- app.py
+++ app.py
@@ -7,3 +7,22 @@ while True:
     if opc_fun == 1:
         menu_Crud()
         opc_crud = int(input('Selecione uma opção: '))
+        #[1] - Adicionar Receita
+        if opc_crud == 1:
+            adicionar_Receita()
+        # [2] - Visualizar Receitas
+        if opc_crud == 2:
+            ver_Receitas()
+        # [3] - Atualizar Receitas
+        if opc_crud == 3:
+            atualizar_Receita()
+        # [4] - Excluir Receitas
+        if opc_crud == 4:
+            deletar_Receita()
+        # [5] - Voltar
+        if opc_crud == 5:
+            menu_Funcionalides()
+        # [6] - Sair
+        if opc_crud == 6:
+            print('Programa Encerrado!')
+            break
